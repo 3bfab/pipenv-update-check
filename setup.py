7368277e77d5aa37b3a2de9dc1837fcbae09a1b7
cuneyt <cuneyt@3bfab2021>
2021-11-08 15:34:57 +0300

    updated.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'src',         # How you named your package folder (MyLib)
-  packages = ['pipenv-update-check'],   # Chose the same as "name"
+  packages = ['src'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Pip environment update check',   # Give a short description about your library
