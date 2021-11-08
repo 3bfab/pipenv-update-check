c8251262c9981ff89dec17459b97dfcd0583c7ca
cuneyt <cuneyt@3bfab2021>
2021-11-08 16:07:22 +0300

    updated

--- setup.py
+++ setup.py
@@ -1,31 +1,20 @@
 from distutils.core import setup
 
 setup(
-  name = 'src',         # How you named your package folder (MyLib)
-  packages = ['src'],   # Chose the same as "name"
+  name = 'pipenv-check',         # How you named your package folder (MyLib)
+  packages = ['pipenv-check'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Pip environment update check',   # Give a short description about your library
   author = 'cuneyt',                   # Type in your name
   author_email = 'cuneyt@3bfab.com',      # Type in your E-Mail
   url = 'https://github.com/CuneytTaha/pipenv-update-check',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/CuneytTaha/pipenv-update-check/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/CuneytTaha/pipenv-update-check/archive/refs/tags/v_0.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'terminaltables',
           'toml',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
   ],
 )
